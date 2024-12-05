59cf48a741f84af5394d7df2a69e4e259b1f873e
MidnightFruit <81094799+MidnightFruit@users.noreply.github.com>
2024-12-05 23:35:28 +0700

    Добавлена документация

--- NetworkManager/urls.py
+++ NetworkManager/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from NetworkManager.views import NetworkNodeViewSet
+from NetworkManager.views import NetworkNodeViewSet, ContactViewSet, ProductViewSet
 from NetworkManager.apps import NetworkmanagerConfig
 
 
@@ -9,6 +9,8 @@ app_name = NetworkmanagerConfig.name
 
 router = DefaultRouter()
 router.register(r'network', NetworkNodeViewSet, basename='network')
+router.register(r'contacts', ContactViewSet, basename='contacts')
+router.register(r'product', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
